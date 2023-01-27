bf0b63a465511485c677f26ce268e940fe4f659f
AfifurRaihan <113502491+AfifurRaihan@users.noreply.github.com>
2023-01-26 21:28:51 -0800

    Initial commit

---
+++ buku-service/src/main/java/com/afifur/buku/service/BukuService.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.afifur.buku.service;
+
+import com.afifur.buku.entity.Buku;
+import com.afifur.buku.repository.BukuRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author ASUS
+ */
+@Service
+public class BukuService {
+    @Autowired
+    private BukuRepository bukuRepository;
+    
+    public Buku saveBuku(Buku buku){
+        return bukuRepository.save(buku);
+    }
+    
+    public Buku findBukuById(Long bukuId){
+        return bukuRepository.findByBukuId(bukuId);
+    }
+
+
+
+}
